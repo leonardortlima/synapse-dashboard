8b880e45d1cc297fa839f59061e6ee731ef0586e
Leonardo Lima <leonardo.lima@ciag.org.br>
2019-03-25 09:35:45 -0300

    remove old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './service/api.service';
-import { from, timer, interval } from 'rxjs';
-import { mergeMap, filter, map, toArray } from 'rxjs/operators';
-import { EChartOption } from 'echarts';
 import { MenuItem } from './component/navbar/navbar.component';
 
 export interface RenderedChart {
@@ -25,47 +21,11 @@ export class AppComponent implements OnInit {
 
   toggleMenu = true;
 
-  public pageTitle = 'Pedidos';
-  public sheetsResponse;
+  constructor() {}
 
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit() {
-    interval(1000).pipe(
-      mergeMap(() => this.apiService.getSheetValues('1s2oRkL9cZh4SreVKVZWn7l-g5pXI8Km_eFvvkCGnzP8', 'A2:B5')),
-      map(response => this.transform(response))
-    ).subscribe(response => this.handleResponse(response));
-  }
+  ngOnInit() { }
 
   onMenuClick(menuItem: MenuItem) {
     this.charts.push(menuItem);
-  }
-
-  private handleResponse(response) {
-    const [countries, values] = response;
-    // this.mergeOption = {
-    //   xAxis: {
-    //     type: 'category',
-    //     data: countries
-    //   },
-    //   series: [
-    //     {
-    //       data: values,
-    //       type: 'line'
-    //     }
-    //   ]
-    // };
-  }
-
-  private transform(apiResponse) {
-    return apiResponse.values.reduce(
-      (acc, it) => {
-        const [countries, values] = acc;
-        countries.push(it[0]);
-        values.push(it[1]);
-        return acc;
-      },
-      [[], []]
-    );
   }
 }
